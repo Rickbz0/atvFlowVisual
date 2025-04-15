99727af73916495e68d4be84de1168a75b727806
Seu Novo Nome <rickhfbo@gmail.com>
2025-04-15 08:38:31 -0300

    atividade 3

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-// Solicita ao usuário que digite um número
+
 let numero = parseInt(prompt("Digite um número:"));
 
-// Verifica se o número é par
+if (isNaN(numero) || numero < 0) {
+  alert("Erro: digite apenas numeros validos.");
+}
 if (numero % 2 === 0) {
     alert("O número é par.");
 } else {

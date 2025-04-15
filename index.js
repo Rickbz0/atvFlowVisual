5a794c72581731328e75cd7b0c5d80116c330592
Seu Novo Nome <rickhfbo@gmail.com>
2025-04-15 08:18:21 -0300

    atv3

---
+++ index.js
@@ -0,0 +1,9 @@
+// Solicita ao usuário que digite um número
+let numero = parseInt(prompt("Digite um número:"));
+
+// Verifica se o número é par
+if (numero % 2 === 0) {
+    alert("O número é par.");
+} else {
+    alert("O número não é par.");
+}
